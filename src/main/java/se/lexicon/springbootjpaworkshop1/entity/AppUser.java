b16442f2621c45e3151277ea55a213ec373a88d4
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-19 11:14:49 +0200

    added constructors and setters

--- src/main/java/se/lexicon/springbootjpaworkshop1/entity/AppUser.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/entity/AppUser.java
@@ -1,4 +1,50 @@
 package se.lexicon.springbootjpaworkshop1.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+//@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString //(exclude = "regDate")
+//@Data
+
+@Entity //(username = "_AppUser")
+//@Table(username = " ")
 public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    @Setter
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    @Setter
+    private String password;
+    @Column(nullable = false, length = 100)
+    @Setter
+    private Details userDetails;
+
+
+    private LocalDateTime regDate;
+
+
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "details_id")
+    private Details details;
+
+    public AppUser(String firstName, String lastName, String email) {
+        this.username = username;
+        this.password = password;
+        this.regDate = LocalDateTime.now();
+    }
 }
--- src/main/java/se/lexicon/springbootjpaworkshop1/entity/Details.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/entity/Details.java
@@ -1,4 +1,28 @@
 package se.lexicon.springbootjpaworkshop1.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Details {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String name;
+    private LocalDateTime birthDate;
+
+    public Details(String email, String name) {
+        this.email = email;
+        this.name = name;
+        this.birthDate =
+
+    }
 }
