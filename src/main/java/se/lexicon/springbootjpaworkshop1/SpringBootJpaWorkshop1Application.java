46b93b9092c9cab64c459cc90aacbee1783d88ea
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-19 08:55:55 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/springbootjpaworkshop1/SpringBootJpaWorkshop1Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootjpaworkshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaWorkshop1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaWorkshop1Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/springbootjpaworkshop1/SpringBootJpaWorkshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootjpaworkshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaWorkshop1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
