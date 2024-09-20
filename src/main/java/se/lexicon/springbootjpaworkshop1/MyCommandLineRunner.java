1f486cd172d1e221f4ea75e2d6a98c12b0125879
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-20 10:27:58 +0200

    modified

--- src/main/java/se/lexicon/springbootjpaworkshop1/MyCommandLineRunner.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/MyCommandLineRunner.java
@@ -3,7 +3,7 @@ package se.lexicon.springbootjpaworkshop1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import se.lexicon.springbootjpaworkshop1.entity.AppUser;
+
 import se.lexicon.springbootjpaworkshop1.repository.AppUserRepository;
 
 @Component
