31539f43954e90dc81a7a33a1f1d617f9705a274
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-20 08:54:52 +0200

    modified

--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/AppUserRepository.java
@@ -3,9 +3,20 @@ package se.lexicon.springbootjpaworkshop1.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.springbootjpaworkshop1.entity.AppUser;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 
 public interface AppUserRepository extends CrudRepository<AppUser, String> {
+
+    Optional<AppUser> findByUsername(String username);
+    Optional<List<AppUser>> findBetweenTwoDates(LocalDate startDate, LocalDate endDate);
+    Optional<AppUser> findByDetailsId(int detailsId);
+    Optional<AppUser> findByEmailIgonreCase(String email);
+
+    //Optional
+    Optional<AppUser> findByUsernameAndPassword(String username, String password);
 }
 
 
--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/detailsRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/detailsRepository.java
@@ -3,5 +3,15 @@ package se.lexicon.springbootjpaworkshop1.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.springbootjpaworkshop1.entity.Details;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface detailsRepository extends CrudRepository<Details, Integer> {
+
+    Optional<Details> findByEmail(String email);
+    Optional<Details> findByNameContains(String name);
+    Optional<Details> findByNameIgnoreCase(String name);
+
+    // Optional
+    Optional<Details> findByBirthDate(LocalDate birthDate);
 }
