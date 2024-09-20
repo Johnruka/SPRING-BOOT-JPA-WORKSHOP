8db3abf67a337dcef077b91a6b9e37debaaf4190
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-20 10:40:32 +0200

    modified

--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/AppUserRepository.java
@@ -19,7 +19,6 @@ public interface AppUserRepository extends CrudRepository<AppUser, String> {
     Optional<AppUser> findByEmailIgonreCase(String email);
 
     Optional<AppUser> findByUsernameAndPassword(String username, String password);
-
 }
 
 
