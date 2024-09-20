3923107ee02c3e10b38e534f50b74ad8bedc45cb
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-20 09:54:53 +0200

    modified

--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/detailsRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/detailsRepository.java
@@ -3,7 +3,6 @@ package se.lexicon.springbootjpaworkshop1.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.springbootjpaworkshop1.entity.Details;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 public interface detailsRepository extends CrudRepository<Details, Integer> {
