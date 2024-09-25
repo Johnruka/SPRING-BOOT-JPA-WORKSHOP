ee11289e1f341f159a49b3c346bd788d5b257aea
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-25 17:32:21 +0200

    added getters, setters and constructors

--- src/main/java/se/lexicon/springbootjpaworkshop1/entity/Author.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/entity/Author.java
@@ -1,4 +1,39 @@
 package se.lexicon.springbootjpaworkshop1.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private int authorId;
+
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private  String lastName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "authors_books",
+            joinColumns = @JoinColumn(name = "auther_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private Set<Author> Authors = new HashSet<>();
 }
