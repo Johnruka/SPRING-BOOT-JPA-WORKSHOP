be2a960a78d09848cbcacc9e44784882632d4064
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-25 10:35:14 +0200

    modified

--- src/main/java/se/lexicon/springbootjpaworkshop1/MyCommandLineRunner.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/MyCommandLineRunner.java
@@ -1,4 +1,29 @@
 package se.lexicon.springbootjpaworkshop1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import se.lexicon.springbootjpaworkshop1.repository.AppUserRepository;
+import se.lexicon.springbootjpaworkshop1.repository.detailsRepository;
+
+;
+
 public class MyCommandLineRunner {
+
+    @Component
+    public class myCommandLineRunner implements CommandLineRunner {
+
+        @Autowired
+        AppUserRepository appUserRepository;
+
+        @Autowired
+        detailsRepository detailsRepository;
+
+        @Override
+        public void run(String... args) throws Exception {
+
+        }
+    }
 }
+
