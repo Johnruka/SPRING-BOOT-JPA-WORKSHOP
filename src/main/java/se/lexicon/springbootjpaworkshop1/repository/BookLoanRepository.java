758bbcde480370e25fb0d8d1174ad55c5b42ca62
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-25 14:12:21 +0200

    Methods added

--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/BookLoanRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/BookLoanRepository.java
@@ -1,7 +1,13 @@
 package se.lexicon.springbootjpaworkshop1.repository;
 
 import org.springframework.data.repository.CrudRepository;
+
 import se.lexicon.springbootjpaworkshop1.entity.BookLoan;
 
+
+
 public interface BookLoanRepository extends CrudRepository<BookLoan, Integer> {
+
 }
+
+
--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/BookRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/BookRepository.java
@@ -3,5 +3,11 @@ package se.lexicon.springbootjpaworkshop1.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.springbootjpaworkshop1.entity.Book;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
+
+    Optional<Book> findByIsbnIgnoreCase(String isbn);
+    List<Book> findByTitleContaining(String title);
 }
