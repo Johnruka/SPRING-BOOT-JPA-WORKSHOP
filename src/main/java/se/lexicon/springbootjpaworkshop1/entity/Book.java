375d020f3c6b029c20fac5177f0e57e26a4a6406
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-25 11:18:01 +0200

    added Getters and setters and manyToOne relationship.

--- src/main/java/se/lexicon/springbootjpaworkshop1/entity/Book.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/entity/Book.java
@@ -1,7 +1,38 @@
 package se.lexicon.springbootjpaworkshop1.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String isbn;
-}
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int maxLoanDays;
+
+
+    }
+
+
+
+
+
+
