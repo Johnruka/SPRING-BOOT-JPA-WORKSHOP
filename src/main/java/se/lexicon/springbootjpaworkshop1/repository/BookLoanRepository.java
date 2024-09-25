1ca1d450d66e39ceaf6d1c85fc10c4f156a9c85d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-25 15:54:29 +0200

    added methods

--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/BookLoanRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/BookLoanRepository.java
@@ -1,12 +1,25 @@
 package se.lexicon.springbootjpaworkshop1.repository;
 
+
 import org.springframework.data.repository.CrudRepository;
 
 import se.lexicon.springbootjpaworkshop1.entity.BookLoan;
 
+import java.time.LocalDate;
+import java.util.List;
 
 
 public interface BookLoanRepository extends CrudRepository<BookLoan, Integer> {
+
+
+    List<BookLoan> findByBorrower_Id(int borrowerId);
+
+
+    List<BookLoan> findByReturnedFalse();
+
+
+    List<BookLoan> findByLoanDateBetween(LocalDate startDate, LocalDate endDate);
+
 
 }
 
