7ad33ca9ea8766f3cab1a4f4ddcaa4bef18355f5
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-26 18:39:14 +0200

    added relationships

--- src/main/java/se/lexicon/springbootjpaworkshop1/entity/Details.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/entity/Details.java
@@ -38,8 +38,7 @@ public class Details {
         @OneToOne(mappedBy = "details")
         AppUser appUser;
 
-        //Constructor
-        // Bypassing the birthDate field for now!
+
     public Details(String name, String email) {
             this.email = email;
             this.name = name;
