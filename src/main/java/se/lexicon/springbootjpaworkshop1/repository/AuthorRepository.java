ee573aa2c41574560e58783eda566c675c3b9f7c
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-26 17:35:04 +0200

    added methods.

--- src/main/java/se/lexicon/springbootjpaworkshop1/repository/AuthorRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop1/repository/AuthorRepository.java
@@ -1,7 +1,30 @@
 package se.lexicon.springbootjpaworkshop1.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.springbootjpaworkshop1.entity.Author;
 
+import java.util.List;
+
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
+
+
+    List<Author> findByFirstName(String firstName);
+
+
+    List<Author> findByLastName(String lastName);
+
+
+    List<Author> findByFirstNameContainingOrLastNameContaining(String firstName, String lastName);
+
+
+    @Modifying
+    @Query("update Author a set a.firstName = :firstName, a.lastName = :lastName where a.authorId = :id")
+    void updateAuthorName(int id, String firstName, String lastName);
+
+
+    //@Modifying
+    //@Query("delete from Author a where a.authorId = :id")
+    //void deleteById(int id);
 }
